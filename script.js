fee2f66a7680b96fba5fa5568405d62a566e02b9
Aline Moraes <120573858+aliiinemoraes@users.noreply.github.com>
2024-02-15 12:21:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,25 +6,30 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
-let novaSenha = "";
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oniput = function(){
-    sizePassword.innerHTML = this.value;
+slider.oninput = function() {
+  sizePassword.innerHTML = this.value;
 }
-function generatePassword(){
-    let pass = "";
 
-    for (let i = 0, n = charset.length; i < sliderElement.value; ++i){
-    pass += charset.charAt(Math.floor(Math.random() * n))
-    }
-    containerPassword.classList.remove("hide");
-    password.innerHTML = pass;
-    novaSenha = pass;
+
+function generatePassword(){
+
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  
+  console.log(pass)
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
 }
+
 function copyPassword(){
-    alert("Senha copiada com sucesso!");
-    navigator.clipboard.writeText(novaSenha);
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
 }
